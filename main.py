8163d0d059cbaa476bcae7c03743a00bf3ced2ab
ZakDeveloperAI <belfakhirzakaria@gmail.com>
2025-07-17 00:42:01 +0200

    added tools and managed tool calling

--- main.py
+++ main.py
@@ -12,11 +12,9 @@ from langgraph.prebuilt import ToolNode
 
 load_dotenv()
 
-llm = ChatGoogleGenerativeAI(
-    model="gemini-2.5-flash",
-    temperature=0.2,
-)
 
+
+#al momento non uso db vettoriale
 document_content=""
 
 class state(TypedDict):
@@ -25,4 +23,34 @@ class state(TypedDict):
 
 @tool #converte funzione python in tool utilizzabile da agente
 def update(content: str) -> str:
-    pass
+    """Updates the document with the provided content."""
+    global document_content
+    document_content = content
+    return f"Document updated successfully. the current content is: \n{document_content}"
+
+@tool
+def save(filename:str)->str: #I'll use simple text for this project no json
+    """Saves the current document content to the specified text filename and finish the process.
+    
+    Args:
+    filename: Name for the text file.
+    """
+    global document_content
+    
+    if not filename.endswith(".txt"):
+        filename += ".txt"
+    
+    
+    try:
+        with open(filename, "w", encoding="utf-8") as f:
+            f.write(document_content)
+        return f"Document saved successfully to {filename}."
+    except Exception as e:
+        return f"Failed to save document: {str(e)}"
+    
+tools=[update,save]
+
+llm = ChatGoogleGenerativeAI(
+    model="gemini-2.0-flash",
+    temperature=0.2,
+).bind_tools(tools)
